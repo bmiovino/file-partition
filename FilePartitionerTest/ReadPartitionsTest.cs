74f0afa17cb005fe770e4f3457a5590640b89d69
Brian M Iovino <bmiovino@gmail.com>
2023-12-31 10:17:41 -0500

    test udpates

---
+++ FilePartitionerTest/ReadPartitionsTest.cs
@@ -0,0 +1,67 @@
+﻿using FilePartitioner;
+using System.Diagnostics;
+
+namespace FilePartitionerTest;
+
+public class ReadPartitionsTest
+{
+    [Fact]
+    public void ReadPartitionDataTest()
+    {
+        #region #------------------------------- Assemble --------------------------------------#
+
+        var directory = @"C:\Temp_File_Partitioner_ReadTests\";
+
+        if (!Directory.Exists(directory))
+            Directory.CreateDirectory(directory);
+
+        var files = Directory.GetFiles(directory, "*.csv");
+        foreach (var file in files)
+        {
+            File.Delete(file);
+        }
+
+
+        var filebase = "file";
+
+        FilePartitioner<TestData> filePartitioner = new FilePartitioner.FilePartitioner<TestData>(new CsvFileReaderWriter<TestData>(), directory, filebase, "csv");
+
+        var data = new List<TestData>();
+
+        for (int i = 0; i < 100; i++)
+            data.Add(new TestData(i));
+
+        var res = filePartitioner.WritePartitions(data, 9);
+
+        Assert.Equal(PartitionActionStatus.ParitionStatusEnum.Success, res.Status);
+
+        #endregion
+
+        #region #------------------------------- Act --------------------------------------#
+        FilePartitioner<TestData> filePartitionerReader = new FilePartitioner.FilePartitioner<TestData>(new CsvFileReaderWriter<TestData>(), directory, filebase, "csv");
+
+        var readRes = new List<PartitionReadResult<TestData>?>();
+
+        for (int i = 0; i < 12; i++)
+        {
+            var readItem = filePartitionerReader.ReadPartition(i);
+            readRes.Add(readItem);
+        }
+
+        #endregion
+
+        #region #------------------------------- Assert --------------------------------------#
+
+        for (int i = 0; i < 12; i++)
+        {
+            Assert.NotNull(readRes[i]);
+            Assert.Equal(PartitionActionStatus.ParitionStatusEnum.Success, readRes[i]!.Status);
+            if (i < 11)
+                Assert.Equal(9, readRes[i]!.Data!.Count());
+            else
+                Assert.Single(readRes[i]!.Data!);
+        }
+
+        #endregion
+    }
+}
--- FilePartitionerTest/WritePartitionsTest.cs
+++ FilePartitionerTest/WritePartitionsTest.cs
@@ -1,112 +1,38 @@
-
 using FilePartitioner;
-using System.Diagnostics;
 
-namespace FilePartitionerTest
+namespace FilePartitionerTest;
+
+public class WritePartitionsTest
 {
-    public class WritePartitionsTest
+    [Fact]
+    public void WritePartitionDataTest()
     {
-        [Fact]
-        public void WritePartitionDataTest()
+        var directory = @"C:\Temp_File_Partitioner_Tests\";
+
+        if (!Directory.Exists(directory))
+            Directory.CreateDirectory(directory);
+
+        //clean out the files
+        var files = Directory.GetFiles(directory, "*.csv");
+        foreach (var file in files)
         {
-            var directory = @"C:\Temp\Test\FP1\";
-
-            if (!Directory.Exists(directory))
-                Directory.CreateDirectory(directory);
-            
-            //clean out the files
-            var files = Directory.GetFiles(directory);
-            foreach(var file in files)
-            {
-                File.Delete(file);
-            }
-
-            var filebase = "file";
-            
-            FilePartitioner<TestData> filePartitioner = new FilePartitioner.FilePartitioner<TestData>(new CsvFileReaderWriter<TestData>(), directory, filebase, "csv");
-            
-            var data = new List<TestData>();
-
-            for(int i = 0; i < 100; i++)
-                data.Add(new TestData(i));
-
-            var res = filePartitioner.WritePartitions(data, 9);
-
-            Assert.Equal(PartitionActionStatus.ParitionStatusEnum.Success, res.Status);
-            Assert.Equal(0, filePartitioner.MinIndex);
-            Assert.Equal(99, filePartitioner.MaxIndex);
-            Assert.Equal(12, filePartitioner.NumberOfPartitions);
+            File.Delete(file);
         }
 
-        [Fact]
-        public void ReadPartitionDataTest()
-        {
-            #region #------------------------------- Assemble --------------------------------------#
+        var filebase = "file";
+        
+        FilePartitioner<TestData> filePartitioner = new FilePartitioner.FilePartitioner<TestData>(new CsvFileReaderWriter<TestData>(), directory, filebase, "csv");
+        
+        var data = new List<TestData>();
 
-            //var directory = @"";
+        for(int i = 0; i < 100; i++)
+            data.Add(new TestData(i));
 
-            //if (!Directory.Exists(directory))
-            //    Directory.CreateDirectory(directory);
+        var res = filePartitioner.WritePartitions(data, 9);
 
-            var process = Process.GetCurrentProcess(); // Or whatever method you are using
-            string processPath = process.MainModule!.FileName!;
-
-            var processParts = processPath.Split('\\');
-            Console.WriteLine("Process Path " + processPath);
-            var directory = string.Join("\\", processParts.Take(processParts.Length - 1));
-
-            //clean out the files
-            if (directory == "")
-                directory = @"/usr/share/dotnet/";
-            
-            var files = Directory.GetFiles(directory, "*.csv");
-            foreach (var file in files)
-            {
-                File.Delete(file);
-            }
-            
-
-            var filebase = "file";
-
-            FilePartitioner<TestData> filePartitioner = new FilePartitioner.FilePartitioner<TestData>(new CsvFileReaderWriter<TestData>(), directory, filebase, "csv");
-
-            var data = new List<TestData>();
-
-            for (int i = 0; i < 100; i++)
-                data.Add(new TestData(i));
-
-            var res = filePartitioner.WritePartitions(data, 9);
-
-            Assert.Equal(PartitionActionStatus.ParitionStatusEnum.Success, res.Status);
-
-            #endregion
-
-            #region #------------------------------- Act --------------------------------------#
-            FilePartitioner<TestData> filePartitionerReader = new FilePartitioner.FilePartitioner<TestData>(new CsvFileReaderWriter<TestData>(), directory, filebase, "csv");
-
-            var readRes = new List<PartitionReadResult<TestData>?>();
-
-            for(int  i = 0; i < 12; i++)
-            {
-                var readItem = filePartitionerReader.ReadPartition(i);
-                readRes.Add(readItem);
-            }
-
-            #endregion
-
-            #region #------------------------------- Assert --------------------------------------#
-
-            for(int i = 0; i < 12; i++)
-            {
-                Assert.NotNull(readRes[i]);
-                Assert.Equal(PartitionActionStatus.ParitionStatusEnum.Success, readRes[i]!.Status);
-                if(i < 11)
-                    Assert.Equal(9, readRes[i]!.Data!.Count());
-                else
-                    Assert.Single(readRes[i]!.Data!);
-            }
-
-            #endregion
-        }
+        Assert.Equal(PartitionActionStatus.ParitionStatusEnum.Success, res.Status);
+        Assert.Equal(0, filePartitioner.MinIndex);
+        Assert.Equal(99, filePartitioner.MaxIndex);
+        Assert.Equal(12, filePartitioner.NumberOfPartitions);
     }
 }
